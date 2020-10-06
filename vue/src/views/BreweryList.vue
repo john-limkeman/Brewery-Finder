5a24abd24d83d810385aa28606d67bf95d96c40e
John Limkeman <john.limkeman@gmail.com>
2020-10-06 12:57:03 -0400

    bootstrap + brewery list css

--- vue/src/views/BreweryList.vue
+++ vue/src/views/BreweryList.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
     <h2>Breweries</h2>
-    <div v-for="brewery in breweries" v-bind:key="brewery.id">
-      <h3>
+    <div v-for="brewery in breweries" v-bind:key="brewery.id" class='brewery'>
         <img v-bind:src='brewery.imgUrl' width="180px" height="auto"/>
-        <router-link v-bind:to="{ name: 'BreweryBeers', params: {id:brewery.id} }">
+      <h3>
+        <router-link v-bind:to="{ name: 'BreweryBeers', params: {id:brewery.id} }" class='brewery-name'>
          
           {{brewery.name}}
           
@@ -35,13 +35,30 @@ export default {
 
 <style>
 .container {
+display: flex;
+flex-direction: column;
   border: 1px, solid, black;
   background-color: wheat;
-  width: 30%;
+  width: 70%;
+  position: fixed;
+  top: 25px;
 }
 
 div > img {
   max-width: 100%;
   max-height: 100%;
+}
+.brewery{
+    display: flex;
+    align-items: center;
+    margin: 15px;
+}
+h2{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    text-align: center;
+    font-weight: bold;
+}
+.brewery-name{
+    color:black;
 }
 </style>
