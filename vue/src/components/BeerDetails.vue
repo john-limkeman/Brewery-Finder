13a805004e85da0979f37c54877e67220ff90480
John Limkeman <john.limkeman@gmail.com>
2020-10-06 10:06:05 -0400

    beer details raw

--- vue/src/components/BeerDetails.vue
+++ vue/src/components/BeerDetails.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
       <h2>{{beer.name}}</h2>
+      <h4>
+          {{beer.brewery_id}} <!-- need to pull from $store to get name-->
+      </h4>
+      <ul>
+          <li>
+              {{beer.type}}
+          </li>
+          <li>
+              {{beer.abv}}
+          </li>
+          <li>
+              {{beer.ibu}}
+          </li>
+          <li>
+               {{this.setStatus}}
+          </li>
+      </ul>
   </div>
 </template>
 
@@ -9,7 +26,18 @@ import breweryServices from "../services/BreweryService.js"
 export default {
     data() {
         return {
-            beer: {}
+            beer: {},
+            status: ""
+        }
+    },
+    computed: {
+        setStatus(){
+            if (this.beer.current){
+                return "This beer is currently on tap!";
+                
+            } else {
+                return "This beer is currently not on tap.";
+            }
         }
     },
     created() {
