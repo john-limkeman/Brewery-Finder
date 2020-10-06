da89229892270c5f574b627a581f456af0c9c2c9
Steven Pennington <spennington45@gmail.com>
2020-10-06 15:45:21 -0400

    Style even more

--- vue/src/components/BeerDetails.vue
+++ vue/src/components/BeerDetails.vue
@@ -1,76 +1,78 @@
 <template>
-  <div class="beerInfo">
-      <h2>{{beer.name}} <span> {{beer.rating}} </span></h2> 
-      <h4 v-bind='getBrewery()'>
-          {{brewery.name}} 
-      </h4>
-      <img v-bind:src='beer.imgUrl' width="180px" height="auto"/>
-      <ul>
-          <li>
-              {{beer.type}}
-          </li>
-          <li>
-              ABV : {{beer.abv}}
-          </li>
-          <li>
-              IBU : {{beer.ibu}}
-          </li>
-          <li>
-               {{this.setStatus}}
-          </li>
-      </ul>
-      <p> {{beer.description}} </p>
+  <div id="beerInfo" class="contaner text-center col-xl-12 mx-auto rounded">
+    <h2>
+      {{ beer.name }} <span> {{ beer.rating }} </span>
+    </h2>
+    <h4 v-bind="getBrewery()">
+      <router-link class="navlink" v-bind:to="{ name: 'BreweryBeers', params: { id: brewery.id } }">
+        {{ brewery.name }}
+      </router-link>
+    </h4>
+    <img v-bind:src="beer.imgUrl" width="180px" height="auto" />
+    <ul>
+      <li>
+        {{ beer.type }}
+      </li>
+      <li>ABV : {{ beer.abv }}</li>
+      <li>IBU : {{ beer.ibu }}</li>
+      <li>
+        {{ this.setStatus }}
+      </li>
+    </ul>
+    <p>{{ beer.description }}</p>
   </div>
 </template>
 
 <script>
-import breweryServices from "../services/BreweryService.js"
+import breweryServices from "../services/BreweryService.js";
 export default {
-    data() {
-        return {
-            beer: {},
-            brewery: '',
-
-        }
+  data() {
+    return {
+      beer: {},
+      brewery: "",
+    };
+  },
+  computed: {
+    setStatus() {
+      if (this.beer.current) {
+        return "This beer is currently on tap!";
+      } else {
+        return "This beer is currently not on tap.";
+      }
     },
-    computed: {
-        setStatus(){
-            if (this.beer.current){
-                return "This beer is currently on tap!";
-                
-            } else {
-                return "This beer is currently not on tap.";
-            }
-        }
+  },
+  methods: {
+    getBrewery() {
+      breweryServices.getBreweryById(this.beer.breweryId).then((response) => {
+        this.brewery = response.data;
+      });
     },
-    methods : {
-        getBrewery(){
-            breweryServices.getBreweryById(this.beer.breweryId).then((response) => {
-                this.brewery = response.data;
-            })
-
-        }
-    },
-    created() {
-        breweryServices.getBeerById(this.$route.params.id).then((response) => {
-            this.beer = response.data;
-            console.log(this.beer.breweryId)
-        })
-        
-    }
-}
+  },
+  created() {
+    breweryServices.getBeerById(this.$route.params.id).then((response) => {
+      this.beer = response.data;
+      console.log(this.beer.breweryId);
+    });
+  },
+};
 </script>
 
 <style>
-.beerInfo{
-
+#beerInfo {
+  background: wheat;
   text-align: center;
   padding: 50px;
   margin: 10px;
   border-style: solid;
+  border-width: 3px;
+  width: 60%;
+  justify-content: center;
+  align-items: center;
 }
-
-
-
-
+li {
+    list-style-type: none;
+}
+div > img {
+    margin: 10px;
+}
 </style>
