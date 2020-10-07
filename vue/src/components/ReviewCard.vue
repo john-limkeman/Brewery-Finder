beaef24bdc7797913ad7e746df05d7b0254a6b8e
John Limkeman <john.limkeman@gmail.com>
2020-10-07 18:23:17 -0400

    done for day

--- vue/src/components/ReviewCard.vue
+++ vue/src/components/ReviewCard.vue
@@ -1,16 +1,17 @@
 <template>
+<div class='revCardContainer'>
   <div class='review-card'>
-      <h4> {{review.title}}</h4>
-      <p>{{review.rating}}</p>
-      <h6>{{this.username}}</h6>
-    <p> {{review.overall}}</p>
-    <p>{{review.color}}</p>
-    <p>{{review.taste}}</p>
-    <p>{{review.head}}</p>
-    <p>{{review.smell}}</p>
-    <p>{{review.reviewDate}}</p>
-    <p>{{review.reviewImgUrl}}</p>
-
+      <h4 id='review-title'> {{review.title}}</h4>
+      <p id='review-rating'>{{review.rating}}</p>
+      <h6 id='review-user'>By: {{this.username}}</h6>
+    <p id='review-overall'> Overall: {{review.overall}}</p>
+    <p id='review-color'>Color: {{review.color}}</p>
+    <p id='review-taste'>Taste: {{review.taste}}</p>
+    <p id='review-head'>Head: {{review.head}}</p>
+    <p id='review-smell'>Smell: {{review.smell}}</p>
+    <p id='review-date'>Submitted On: {{review.reviewDate}}</p>
+    <p id='review-img'>{{review.reviewImgUrl}}</p>
+</div>
   </div>
 </template>
 
@@ -30,5 +31,57 @@ export default {
 </script>
 
 <style>
-
+.review-card{
+    display: grid;
+    grid-template-columns: 1fr 2fr 2fr;
+    grid-template-areas: 
+    "title title rating"
+    "username . img"
+    "overall overall img"
+    "color color img"
+    "smell smell img"
+    "taste taste img"
+    "head head img"
+    ". date date";
+    text-align:left;
+    border: solid 1px black;
+    border-radius: 10px;
+    margin: 2px;
+    background-color:ivory;
+    padding: 4px;
+}
+#review-title{
+    grid-area: title;
+}
+#review-rating{
+    grid-area: rating;
+}
+#review-user{
+    grid-area: username;
+}
+#review-overall{
+    grid-area: overall;
+}
+#review-img{
+    grid-area: img;
+}
+#review-color{
+    grid-area: color;
+}
+#review-smell{
+    grid-area: smell;
+}
+#review-taste{
+    grid-area: taste;
+}
+#review-head{
+    grid-area: head;
+}
+#review-date{
+    grid-area: date;
+    text-align: right;
+}
+.revCardContainer{
+    display: block;
+}
 </style>
