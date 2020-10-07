5aa31c55ac099b020126391f0e6e625f0870de9a
John Limkeman <john.limkeman@gmail.com>
2020-10-07 14:28:04 -0400

    set up review components

---
+++ vue/src/components/ReviewList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h3> Reviews </h3>
+      <div>
+          <!-- to contain reviewCards -->
+      </div>
+      <div>
+          <ReviewForm /> 
+      </div>
+  </div>
+</template>
+
+<script>
+import BreweryService from '@/services/BreweryService.vue'
+import ReviewForm from '@/components/ReviewForm.vue'
+export default {
+    data(){
+        return{
+            reviews: []
+        }
+    },
+    components: {
+        ReviewForm
+    },
+    created(){
+        BreweryService
+    }
+}
+</script>
+
+<style>
+
+</style>
