aa850cd16ccf710a03673815980d584ebb4a5aa5
John Limkeman <john.limkeman@gmail.com>
2020-10-09 12:43:16 -0400

    lunch time

--- vue/src/components/BrewerInsider.vue
+++ vue/src/components/BrewerInsider.vue
@@ -1,45 +1,46 @@
 <template>
   <div>
-      <div id='BrewBeerList'>
-          <h4> {{brewery.name}} Beers</h4>
-          <div v-for='beer in beers' v-bind:key='beer.id'>
-              {{beer.name}}
-          </div>
+    <div id="BrewBeerList">
+      <h4>{{ brewery.name }} Beers</h4>
+      <div v-for="beer in beers" v-bind:key="beer.id">
+        {{ beer.name }}
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import BreweryService from '@/services/BreweryService.js'
+import BreweryService from "@/services/BreweryService.js";
 export default {
-    data(){
-        return {
-        beers : [],
-        brewery : {}
+  data() {
+    return {
+      beers: [],
+      brewery: {},
+    };
+  },
+  computed: {},
+  methods: {
+    getBeers() {
+      let mybeers = [];
+      BreweryService.getBeerByBrewery(this.brewery.id).then((response) => {
+        for (let i = 0; i < response.data.length; i++) {
+          mybeers[i] = response.data[i];
         }
+      })
+      return mybeers;
     },
-    methods:{
-        
-    },
-    created(){
-     BreweryService.getBreweryByBrewer(this.$store.state.user.id).then(
-         (response) => {
-             this.brewery = response.data;
-         }
-     ),
-     BreweryService.getBeerByBrewery(this.brewery.id).then(
-         (response) => {
-             for (let i = 0; i < response.data.length; i++){
-
-                 this.beers[i] = response.data[i];
-                
-             }
-         }
-     )
-    }
-}
+  },
+  created() {
+    BreweryService.getBreweryByBrewer(this.$store.state.user.id).then(
+      (response) => {
+        this.brewery = response.data;
+        BreweryService.getBeerByBrewery(this.brewery.id).then((response) => {
+          this.beers = response.data
+      })
+      });
+  },
+};
 </script>
 
 <style>
-
 </style>
