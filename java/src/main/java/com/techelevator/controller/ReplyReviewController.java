7608a3fbc8f68f575710fb820bdd3f907464d909
Brad <bsller74@gmail.com>
2020-10-12 13:31:49 -0400

    added replys for reviews backend

---
+++ java/src/main/java/com/techelevator/controller/ReplyReviewController.java
@@ -0,0 +1,56 @@
+package com.techelevator.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.techelevator.dao.ReplyReviewDAO;
+import com.techelevator.model.ReplyReview;
+
+public class ReplyReviewController {
+	
+	@Autowired
+	ReplyReviewDAO dao;
+	
+	@RequestMapping(path = "/replies", method = RequestMethod.GET)
+	public List<ReplyReview> getAllBeer() {
+		return dao.getAllReplys();
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping(path = "/replies", method = RequestMethod.DELETE)
+	public String deleteReplyReview(@Valid @RequestBody ReplyReview replyReview) {
+		dao.deleteReplyReview(replyReview);
+		return "Reply Deleted";
+	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@RequestMapping(path = "/replies", method = RequestMethod.POST)
+	public void addReplyReview(@Valid @RequestBody ReplyReview replyReview) {
+		dao.addReplyReview(replyReview);
+	}
+	
+	@RequestMapping(path = "/user/{userId}/replies", method = RequestMethod.GET)
+	public List<ReplyReview> getReplysByUserId(@PathVariable Long userId){
+		return dao.getReplysByUserId(userId);
+	}
+	
+	@RequestMapping(path = "/review/{reviewId}/replies", method = RequestMethod.GET)
+	public List<ReplyReview> getReplysByReviewId(@PathVariable Long reviewId){
+		return dao.getReplysByUserId(reviewId);
+	}
+	
+	
+	
+	
+
+}
