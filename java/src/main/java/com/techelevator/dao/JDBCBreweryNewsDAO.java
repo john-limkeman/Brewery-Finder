5255787cdb888947601ddaab839b3c3dc8d54fc3
Brad <bsller74@gmail.com>
2020-10-15 11:24:26 -0400

    documentation

--- java/src/main/java/com/techelevator/dao/JDBCBreweryNewsDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCBreweryNewsDAO.java
@@ -2,13 +2,10 @@ package com.techelevator.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-
-import com.techelevator.model.BrewerRequest;
 import com.techelevator.model.BreweryNews;
 
 @Component
