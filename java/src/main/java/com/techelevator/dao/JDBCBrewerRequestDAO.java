c814b34c31b025a30129853c32cf23aaae8663e4
Brad <bsller74@gmail.com>
2020-10-15 10:46:27 -0400

    documentation

--- java/src/main/java/com/techelevator/dao/JDBCBrewerRequestDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCBrewerRequestDAO.java
@@ -1,14 +1,9 @@
 package com.techelevator.dao;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-
 import com.techelevator.model.BrewerRequest;
-
 
 @Component
 public class JDBCBrewerRequestDAO implements BrewerRequestDAO {
@@ -17,14 +12,14 @@ public class JDBCBrewerRequestDAO implements BrewerRequestDAO {
 @Autowired
 private JdbcTemplate jdbcTemplate;
 
-
+	// Changes processed status in database from the database id
 	@Override
 	public void switchProcessedStatus(BrewerRequest request) {
 		String sql = "UPDATE brewer_request SET processed = ? WHERE id = ?";
 		jdbcTemplate.update(sql, request.isProcessed(), request.getId() );
 	}
 
-
+	// set brewer id to their own brewery in the database
 	@Override
 	public void updateBrewerIdForBrewery(BrewerRequest request) {
 		String sql = "Update breweries SET brewer_id = ? WHERE id = ?";
