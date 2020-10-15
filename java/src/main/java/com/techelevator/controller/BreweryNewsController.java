5776e25c509187376596226d471c71dcc238921b
Brad <bsller74@gmail.com>
2020-10-15 09:59:21 -0400

    documentation

--- java/src/main/java/com/techelevator/controller/BreweryNewsController.java
+++ java/src/main/java/com/techelevator/controller/BreweryNewsController.java
@@ -2,13 +2,13 @@ package com.techelevator.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.techelevator.dao.BreweryNewsDAO;
 import com.techelevator.model.BreweryNews;
 
@@ -19,17 +19,17 @@ public class BreweryNewsController {
 	@Autowired
 	public BreweryNewsDAO dao;
 	
-	
+	// controller to list all news
 	@RequestMapping(path = "/allNews", method = RequestMethod.GET)
 	public List<BreweryNews> getAllNews() {
 		return dao.getAllNews();
 	}
-	
+	// controller to list news by database id
 	@RequestMapping(path = "/news/{id}", method = RequestMethod.GET)
 	public List<BreweryNews> newsByBreweryID(@PathVariable Long id){
 		return dao.getNewsByBreweryId(id);
 	}
-	
+	// controller to delete news by database id
 	@RequestMapping(path = "/news/{id}", method = RequestMethod.DELETE)
 	public void deleteNews(@PathVariable Long id){
 		dao.deleteNews(id);
