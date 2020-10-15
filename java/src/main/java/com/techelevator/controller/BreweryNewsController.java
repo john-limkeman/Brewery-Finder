21998847b1db42c8aad10defc2ac63946aaafa98
Brad <bsller74@gmail.com>
2020-10-15 10:31:41 -0400

    documentation

--- java/src/main/java/com/techelevator/controller/BreweryNewsController.java
+++ java/src/main/java/com/techelevator/controller/BreweryNewsController.java
@@ -1,8 +1,6 @@
 package com.techelevator.controller;
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
